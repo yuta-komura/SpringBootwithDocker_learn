0a919d5137ead1845343ca3297353c8da43bd5f7
yuta-komura <esfgshdfsd@gmail.com>
2024-08-12 04:56:38 +0900

    commit

---
+++ db/initdb.d/user.sql
@@ -0,0 +1,7 @@
+CREATE TABLE user (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
+insert into user values (null,'test_user01');
+commit;
--- project1/src/main/java/com/example/spring_boot_docker/SpringBootDockerApplication.java
+++ project1/src/main/java/com/example/spring_boot_docker/SpringBootDockerApplication.java
@@ -1,17 +1,28 @@
 package com.example.spring_boot_docker;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class SpringBootDockerApplication {
 
+    private final UserService userService;
+
+    @Autowired
+    public SpringBootDockerApplication(UserService userService) {
+        this.userService = userService;
+    }
+
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World project1";
+        List<User> users = userService.selectById();
+        return "Hello Docker World project1 user is " + users.getFirst().name;
     }
 
 
