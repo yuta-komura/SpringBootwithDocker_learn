efe15d5c2b37f47f78d91e1e81d18ef50d3cf976
yuta-komura <esfgshdfsd@gmail.com>
2024-09-01 23:07:59 +0900

    commit

--- prettier.config.js
+++ prettier.config.js
@@ -1,13 +1,24 @@
 module.exports = {
-  printWidth: 80,             // 1行の最大文字数（これ以上の文字数は改行される）
-  tabWidth: 2,                // インデントのスペース数（2スペースを使用）
-  useTabs: false,             // タブではなくスペースを使用
-  semi: true,                 // 行末にセミコロンを付ける
-  singleQuote: true,          // シングルクォートを使用
-  trailingComma: "es5",       // ES5に準拠した末尾のカンマ（オブジェクトや配列の最後にカンマを付ける）
-  bracketSpacing: true,       // オブジェクトリテラルの中括弧内にスペースを追加
-  jsxSingleQuote: false,      // JSXでダブルクォートを使用
-  arrowParens: "always",      // アロー関数の引数が1つの場合でも括弧を付ける
-  endOfLine: "lf",            // 改行コードをLFに統一
-  plugins: ["prettier-plugin-sql"],  // SQLフォーマット用のプラグイン
+  // 1行の最大文字数（これ以上の文字数は改行される）
+  printWidth: 80,
+  // インデントのスペース数（2スペースを使用）
+  tabWidth: 2,
+  // タブではなくスペースを使用
+  useTabs: false,
+  // 行末にセミコロンを付ける
+  semi: true,
+  // シングルクォートを使用
+  singleQuote: true,
+  // ES5に準拠した末尾のカンマ（オブジェクトや配列の最後にカンマを付ける）
+  trailingComma: 'es5',
+  // オブジェクトリテラルの中括弧内にスペースを追加
+  bracketSpacing: true,
+  // JSXでダブルクォートを使用
+  jsxSingleQuote: false,
+  // アロー関数の引数が1つの場合でも括弧を付ける
+  arrowParens: 'always',
+  // 改行コードをLFに統一
+  endOfLine: 'lf',
+  // SQLフォーマット用のプラグイン
+  plugins: ['prettier-plugin-sql'],
 };
